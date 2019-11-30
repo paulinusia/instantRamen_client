190da3b719e59ae6817f3e179399a59289a4fcd8
paulinusia <padamski@luc.edu>
2019-11-29 19:15:49 -0600

    product progress
    Signed-off-by: paulinusia <padamski@luc.edu>

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+   
+    <!-- nav bar if necessary -->
+
+     
+
+    <v-content>
+      
+      <product/>
+
+      <v-spacer/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import Product from './components/Product.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+   
+    product: Product,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Product.vue
@@ -0,0 +1,133 @@
+<template>
+<div class="product">
+
+
+
+
+ <!-- GET METHODS FOR PRODUCT -->
+    <v-row 
+    justify="space-around"
+  class="row">
+
+      <v-card>
+          <v-flex>
+              <input v-model="productID" placeholder="enter Product ID">
+          </v-flex>
+
+          <v-flex> 
+            <p> Product info: {{productID}} </p>  
+          </v-flex>
+          <v-flex > 
+            <v-btn @click="getProductByID"> Get Product By ID </v-btn> 
+          </v-flex>
+      </v-card>
+
+      <v-card>
+          <v-flex>
+            <input v-model="partnerID" placeholder="enter partner ID">
+          </v-flex>
+          <v-flex>
+              <p> partner info: {{partnerID}} </p>
+          <v-flex>
+            <v-btn @click="getProductByPartnerID"> Return prodcuts by partner ID </v-btn>
+          </v-flex>
+          </v-flex>
+      </v-card>
+
+      <v-card>
+        <v-flex>
+          <input v-model="productName" placeholder="enter name of product">
+        </v-flex>
+        <v-flex>
+          <p> product name: {{productName}} </p>
+        </v-flex>
+        <v-flex>
+          <v-btn @click="getProductByName"> Return products by Name </v-btn>
+        </v-flex>
+      </v-card>
+
+      <v-card >
+       <v-flex>
+         <v-btn @click="getProducts"> Get ALL Products </v-btn>
+        </v-flex>
+        
+      </v-card>
+
+    </v-row>
+
+
+   
+
+
+
+ 
+
+ 
+
+<!-- POST/PUT/DELETE FOR AXIOS?? -->
+<v-form>
+
+</v-form>
+
+
+    </div>
+</template>
+
+
+
+<script>
+
+const axios = require('axios');
+
+export default {
+  data() {
+    return {
+      isShowing: false,
+      product: null,
+      productID: null,
+      PartnerProducts: null,
+      partnerID: null,
+      productName: null,
+
+      
+    };
+  },
+  methods: {
+    getProducts() {
+     axios
+      .get('http://localhost:8081/productservice/products/')
+      .then(response => (this.product = response));
+    },
+    getProductByID(productID){  
+     axios.get('http://localhost:8081/productservice/product/'+ this.productID)
+              .then(response => (this.product = response.data));
+    console.log(productID);
+  },
+  getProductByPartnerID(partnerID){  
+     axios.get('http://localhost:8081/productservice/partnerproducts/'+ this.partnerID)
+              .then(response => (this.partnerProducts = response.data));
+    console.log(partnerID);
+  },
+  getProductByName(productName){  
+     axios.get('http://localhost:8081/productservice/products/'+ this.productName)
+              .then(response => (this.productName = response.data));
+    console.log(ProductName);
+
+
+},
+
+
+
+
+
+},
+}
+
+</script>
+
+<style scoped>
+.row{
+  margin-top: 5%;
+  text-align:left;
+}
+</style>
