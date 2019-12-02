8bc98482614701d36349adaa6a3a4083851f8ba8
paulinusia <padamski@luc.edu>
2019-12-02 17:25:52 -0600

    buy product

--- src/components/BuyProduct.vue
+++ src/components/BuyProduct.vue
@@ -1,11 +1,75 @@
 <template>
 <div>
-  <h1> Buy products / display list of all products (getallproducts upon render) here (use mounted life cycle method ) </h1>
-</div>
+   <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                   
+                    <div class="card-body">
+                        <form @submit="formSubmit">
+                        <strong>Product ID</strong>
+                        <input type="text" class="form-control" v-model="fk_product">
+                        <strong>Purchase Detail:</strong>
+                        <textarea class="form-control" v-model="purchasedetail"></textarea>
+    
+                        <button class="btn btn-success">Submit</button>
+                        </form>
+                        <strong>Ordering Result:</strong>
+                        <pre>
+                        {{output}}
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+data(){
+  return{
+  fk_purchasepayment: 109,
+  fk_purchase_owner:104,
+  fk_address:101,
+  purchasestatus: 'Ordered',
+  purchasedetail: '',
+  fk_product: '',
+  output: ''
+  };
+},
+methods:{
+  formSubmit(e) {
+                e.preventDefault();
+                let currentObj = this;
+                this.axios.post('http://localhost:8081/purchaseservice/purchase', {
+                    purchasestatus: this.purchasestatus,
+                    purchasedetail: this.purchasedetail,
+                    fk_product: this.fk_product,
+                    fk_purchase_payment: this.payment,
+                    fk_purchase_owner: this.fk_purchase_owner,
+                    fk_address: this.fk_address,
+                })
+                .then(function (response) {
+                    currentObj.output = response.data;
+                })
+                .catch(function (error) {
+                    currentObj.output = error;
+                });
+            }
+        
+
+},
+created(){
+     
+     axios
+      .get('http://localhost:8081/productservice/products/')
+      .then(response => {this.product = console.log(response)});
+    
+},
+
   
 }
 </script>
