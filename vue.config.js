2e7ff79a0e02a63da52e054239aaa45b42042af5
paulinusia <padamski@luc.edu>
2019-12-08 17:51:40 -0600

    CORS fix attempt

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     'vuetify',
   ],
   devServer: {
-    proxy: 'http://localhost:8081/productservice/products'
+    proxy: 'http://localhost:8081/',
+    headers: { "Access-Control-Allow-Origin": "*" }
     }
   
 };
