2f37c79debb412cdca3f60410e19dc3e88334d43
Linette V Maliakal <lmaliakal21@gmail.com>
2019-12-10 01:44:20 -0600

    search product by name functional
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/components/search/Product.vue
+++ src/components/search/Product.vue
@@ -1,12 +1,6 @@
 <template>
 <div class="product">
 
-
-<Nav />
-<v-container>
-    <v-row 
-    justify="space-around"
-  class="row">
       <v-card>
         <v-flex>
           <input v-model="productName" placeholder="enter name of product">
@@ -19,7 +13,7 @@
           <v-btn @click="getProductByName"> Return Products by Name </v-btn>
         </v-flex>
       </v-card>
-    </v-row>
+    <br />
 <!--<div class="getproductsbypartner" v-if="partnerProducts">
 <strong>All Customers:</strong>  
   <ul v-for="product in partnerProducts" :key="product.id">
@@ -30,34 +24,36 @@
     <br />Description:{{product.productDescription}} <br />
     <br /> Partner ID :{{product.partnerId}} <br />
   </ul>
-</div> -->   
- 
-      <br />
-          <div v-if="productNames">
-             <h1> Product by Name: </h1>
-            Product ID: {{productNames.id}}
-            <br />
-            Name: {{productNames.productName}}
-            <br />
-            Cost: {{productNames.cost}}
-            <br />
-            Description: {{productNames.productDescription}}
-            <br />
-            Partner ID: {{productNames.partnerId}}
-        
-          </div>
-<v-form>
-
-</v-form>
-
-</v-container>
+</div> --> 
+    <div class="container">
+      <table id="productTable">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Cost</th>
+          <th>Description</th>
+          <th>Owner ID</th>
+        </tr>
+        <router-link
+          class="routerLink"
+          v-for="product in products"
+          :key="product.id"
+          :to="{ name: 'product', params: { productID: product.id } }"
+        >
+          <tr class="productRow">
+            <td>{{product.id}}</td>
+            <td>{{product.productName}}</td>
+            <td>{{product.cost}}</td>
+            <td>{{product.productDescription}}</td>
+            <td>{{product.partnerId}}</td>
+          </tr>
+        </router-link>
+      </table>
+    </div>
     </div>
 </template>
 
-
-
 <script>
-
 const axios = require('axios');
 import Nav from '@/components/search/Nav.vue'
 export default {
@@ -75,7 +71,7 @@ export default {
     };
   },
   components:{
-    Nav
+    // Nav
   },
   methods: {
     getProducts() {
@@ -94,8 +90,10 @@ export default {
     }, 
     getProductByName(productName){  
      axios.get('http://localhost:8081/productservice/products/'+ this.productName)
-              .then(response => {this.productNames = response.data.Product[0]
-              console.log(response.data.Product[0])});
+              .then(response => {
+                this.products = response.data.Product;
+                console.log(response.data.Product);
+              });
     },
   },
 }
@@ -108,5 +106,34 @@ export default {
   margin-bottom: 5%;
   margin-left: 5%;
   text-align: center;
+}
+tr {
+  display: flex;
+  justify-content: space-between;
+}
+th {
+  width: 15vw;
+  text-align: left;
+}
+td {
+  width: 15vw;
+  text-align: left;
+}
+.container {
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: center;
+}
+
+#productTable {
+  width: 95vw;
+}
+.routerLink {
+  color: black;
+  text-decoration: none;
+}
+.productRow:hover {
+  background-color: #999;
 }
 </style>
