9d254809f8d2cdaa1c90dade6721c40e0f62b380
Linette V Maliakal <lmaliakal21@gmail.com>
2019-12-09 23:14:22 -0600

    renders individual Product
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

---
+++ src/components/Product/ProductIndividual.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h3>Individual Product Info</h3>
+    <br />
+    ID: {{product.id}}
+    <br />
+    Name: {{product.productName}}
+    <br />
+    Cost: {{product.cost}}
+    <br />
+    Description: {{product.productDescription}}
+    <br />
+    Owner ID: {{product.partnerId}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    console.log("created...");
+    console.log("productID:");
+    console.log(this.productID);
+    this.getProductByID();
+  },
+  data() {
+    return {
+      product: undefined
+    };
+  },
+  methods: {
+    getProductByID() {
+      axios
+        .get(
+          "http://localhost:8081/productservice/product/" + this.productID
+        )
+        .then(response => {
+          console.log("getProductById Response:");
+          console.log(response);
+          this.product = response.data.Product;
+        });
+    }
+  },
+  props: ["productID"]
+};
+</script>
+
+<style scoped>
+</style>
