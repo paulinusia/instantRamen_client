b2843fd1ac6ab7358fa11fa732bc737705d10828
Linette V Maliakal <lmaliakal21@gmail.com>
2019-12-09 21:08:52 -0600

    update form box UI
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
+<div class="col-md-8">
   <div class="home">
     <img src="https://instantpotchef.ca/wp-content/uploads/2018/01/Instant-Pot-Chef-Logo_Main.jpg" height="30%" width="30%">
     <!--<h1> Create customer ??????? is that needed here </h1>-->
     <p> </p>
-    <h1>Signup</h1>
     <customer/> 
     <!--<hr>
     <div class="form-group">
@@ -21,8 +21,8 @@
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
       <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
     </div>-->
- <v-form  class="form" @submit="formSubmit">
-                       
+ <v-form class="form" @submit="formSubmit">
+                       <h1>Signup</h1>
                         <strong>New Customer</strong>
                       
 
@@ -48,6 +48,7 @@
                         {{output}}
                         </pre>
     <!--<p>Message is: {{ message }}</p>-->
+  </div>
   </div>
   </div>
 </template>
@@ -97,4 +98,16 @@ export default {
 }
 </script>
 <style lang="scss">
+</style>
+<style scoped>
+.form {
+  margin: 0 auto;
+  width: 100%;
+  border-style: dotted;
+  border-width: 1px;
+  padding: 5%;
+  margin-right: 5%;
+  /*position: absolute;*/
+  /*margin-left: 35%;*/
+}
 </style>
