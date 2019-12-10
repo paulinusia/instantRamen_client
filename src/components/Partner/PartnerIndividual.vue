1daa97efe5e7c23f1072560cff41d0b8750cf7e0
Linette V Maliakal <lmaliakal21@gmail.com>
2019-12-09 23:56:11 -0600

    update and create Partner
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/components/Partner/PartnerIndividual.vue
+++ src/components/Partner/PartnerIndividual.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
     <h3>Individual Partner Info</h3>
+    <p>
+      ID: {{partner.id}}
+      <br />
+      Name: {{partner.partnerName}}
+      <br />
+      Description: {{partner.description}}
+      <br />
+      Partner Type: {{partner.partnerType}}
+      <br />
+      Phone: {{partner.phoneNumber}}
+    </p>
+    <div id="updatePartnerForm">
+      <v-text-field v-model="updatedPartnerName" label="Updated Partner Name..."></v-text-field>
+      <v-text-field v-model="updatedDescription" label="Updated Description..."></v-text-field>
+      <v-text-field v-model="updatedPartnerType" label="Updated Partner Type..."></v-text-field>
+      <v-text-field v-model="updatedPhoneNumber" label="Updated Phone Number..."></v-text-field>
+      <v-btn outlined @click="updatePartner">Update Partner</v-btn>
+    </div>
     <br />
-    ID: {{partner.id}}
-    <br />
-    Name: {{partner.partnerName}}
-    <br />
-    Description: {{partner.description}}
-    <br />
-    Partner Type: {{partner.partnerType}}
-    <br />
-    Phone: {{partner.phoneNumber}}
+    <div>
+      <v-btn class="createButton" @click="createPartner">Create Partner</v-btn>
+    </div>
   </div>
 </template>
 
@@ -29,6 +41,9 @@ export default {
     };
   },
   methods: {
+    createPartner() {
+      this.$router.push("/");
+    },
     getPartnerByID() {
       axios
         .get(
@@ -39,6 +54,43 @@ export default {
           console.log(response);
           this.partner = response.data.Partner;
         });
+    },
+        updatePartner() {
+      console.log("Updating partner...");
+      let currentObj = this;
+      let updatePartnerLink = "";
+      for (var i = 0; i < currentObj.partner.links.length; i++) {
+        if (currentObj.partner.links[i].rel === "updatePartner") {
+          updatePartnerLink = currentObj.partner.links[i].uri;
+          break;
+        }
+      }
+      console.log("update partner link: " + updatePartnerLink);
+      if (updatePartnerLink.length < 1) return null;
+      axios
+        .put("http://localhost:8081" + updatePartnerLink, {
+          Partner: {
+            id: currentObj.partner.id,
+            partnerName: currentObj.updatedPartnerName,
+            description: currentObj.updatedDescription,
+            partnerType: currentObj.updatedPartnerType,
+            phoneNumber: currentObj.updatedPhoneNumber
+          }
+        })
+        .then(function(response) {
+          console.log("currentObj:");
+          console.log(currentObj);
+          console.log(response);
+          currentObj.partner = response.data.Partner;
+          console.log(currentObj.partner);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      currentObj.updatedPartnerName = "";
+      currentObj.updatedDescription = "";
+      currentObj.updatedPartnerType = "";
+      currentObj.updatedPhoneNumber = "";
     }
   },
   props: ["partnerID"]
@@ -46,4 +98,13 @@ export default {
 </script>
 
 <style scoped>
+#updatePartnerForm {
+  width: 40vw;
+  margin-left: auto;
+  margin-right: auto;
+}
+.createButton {
+  background-color: green !important;
+  color: white !important;
+}
 </style>
